d52bbb0adfcf856ed2a120d1ca204e2ea0d836ee
DarkVooDoo <moisestazaro@gmail.com>
2020-02-28 23:20:11 +0100

    First Commit

--- src/redux/reducers/defaultReducer.js
+++ src/redux/reducers/defaultReducer.js
@@ -1,7 +1,7 @@
 import {DEFAULT_ACTION} from '../actions/types'
 
 // default function to display redux action format
-export default (state = '', action)=>{
+export default (state = 'Default Props', action)=>{
 
     switch(action.type) {
         case DEFAULT_ACTION:      
